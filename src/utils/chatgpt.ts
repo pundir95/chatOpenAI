10a0dc49f0b653b61d8ac9bc214ae616e35dc7bf
pankaj95 <pankaj@avioxtechnologies.com>
2023-06-09 19:11:31 +0530

    Added new API keys

--- src/utils/chatgpt.ts
+++ src/utils/chatgpt.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 export const openai = new OpenAIApi(configuration);
